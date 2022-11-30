43786ba31947f761f22c3295195e55933d93c3d9
SahilShameerDev <smsahilms346@protonmail.com>
2022-11-30 19:15:23 +0530

    Prototype completed

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET'] = 'secret123'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@socketio.on('message')
+def handle_message(message):
+    print("Received:" + message)
+    if message != "User Connected":
+        send(message, broadcast=True)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='localhost')
